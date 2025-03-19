77c3c9b71a2eead2960a6c7cd3b2710535344700
shira-kurztag <38328276852@mby.co.il>
2025-03-19 17:02:30 +0200

    try

--- IBEXDATA/Mapper.cs
+++ IBEXDATA/Mapper.cs
@@ -21,7 +21,7 @@ namespace Application
             CreateMap<BankDTO, Bank>();
             CreateMap<Bank, BankNamesDTO>();
 
-            CreateMap<Contractor, ContractorDTO>();
+            CreateMap<ContractorDTO2, Contractor>().ReverseMap();
             CreateMap<Building, BuildingDTO>();
             CreateMap<Mortagege, MortagegeDTO>().ReverseMap();
 
