f4fcffab639f295139bdd0036c7ef0d56f47a7b0
shira-kurztag <38328276852@mby.co.il>
2025-03-24 15:17:50 +0200

    ff

--- Service/IApartmentService.cs
+++ Service/IApartmentService.cs
@@ -14,6 +14,6 @@ namespace Service
         IEnumerable<Warehouse> GetWarehouseByBuilding(int buildingId);
         IEnumerable<Parking> GetParkingByBuilding(int buildingId);
         IEnumerable<LinkageCode> GetLinkagCode();
-        //xx
+        //xxbb
     }
 }
